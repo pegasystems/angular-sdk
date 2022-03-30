5cec3ccfc3ed01f86f04e1ef4f1f2bf162b139a7
4manasa <manasamashetty96@gmail.com>
2022-03-30 22:09:19 +0530

    US-465950 replaced textinput field with ngx-intl-tel-input widget in phone component (#49)
    * US-465950 replaced textinput with ngx-intl-tel-input component
    
    * fixed build errors
    
    * modified as per PR comments
    
    * removed extra lines
    
    Co-authored-by: manasa <manasa.mashetty@in.pega.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -130,7 +130,7 @@ import { SimpleTableComponent } from './_components/_templates/simple-table/simp
 import { ReferenceComponent } from './_components/reference/reference.component';
 import { OneColumnTabComponent } from './_components/_templates/one-column-tab/one-column-tab.component';
 import { AuthService } from './_services/auth.service';
-
+import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -259,6 +259,7 @@ import { AuthService } from './_services/auth.service';
     MatTooltipModule,
     MatDialogModule,
     MatStepperModule,
+    NgxIntlTelInputModule
   ],
   providers: [
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { floatLabel: 'auto'} },
