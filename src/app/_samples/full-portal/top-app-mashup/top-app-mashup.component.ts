923b3cb1cefc939e1629de70e8bd1fd8166fdeef
vishal sharma <vishalshrm539@gmail.com>
2023-01-05 20:32:57 +0530

    Resolved conflicts issues

--- src/app/_samples/full-portal/top-app-mashup/top-app-mashup.component.ts
+++ src/app/_samples/full-portal/top-app-mashup/top-app-mashup.component.ts
@@ -243,7 +243,7 @@ export class TopAppMashupComponent implements OnInit {
 
     } );
 
-   
+    const thePortal = this.scservice.getAppPortal();
     const defaultPortal = window.PCore?.getEnvironmentInfo?.().getDefaultPortal?.();
     
     // Note: myLoadPortal and myLoadDefaultPortal are set when bootstrapWithAuthHeader is invoked
