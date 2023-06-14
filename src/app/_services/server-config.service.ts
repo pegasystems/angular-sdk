09be192bef658ca9c3c9bfcdcc24e926bb6c1e44
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2023-06-14 16:27:17 +0530

    Showing list of available portals from access group for Operator

--- src/app/_services/server-config.service.ts
+++ src/app/_services/server-config.service.ts
@@ -102,84 +102,72 @@ export class ServerConfigService {
   }
 
   /**
-   * If sdkConfig.serverConfig.appPortal is set, leave it and the specified portal will be used.
-   * If not set, set sdkConfig.serverConfig.appPortal to default portal of currently logged in user
+   * Get available portals which supports SDK
+   * @returns list of available portals (portals other than excludingPortals list)
    */
-  async selectPortal() {
-    if (!this.PCore) {
-      this.PCore = window.PCore;
-    }
+  async getAvailablePortals() {
+    return new Promise((resolve, reject) => {
+      if (!this.PCore) {
+        this.PCore = window.PCore;
+      }
 
-    const serverConfig = this.getSdkConfigServer();
+      const serverConfig = this.getSdkConfigServer();
 
-    if (serverConfig.appPortal !== '' && serverConfig.appPortal !== undefined) {
-      // use the specified portal
-      console.log(`Using appPortal: ${serverConfig.appPortal}`);
-      return;
-    }
+      const userAccessGroup = this.PCore.getEnvironmentInfo().getAccessGroup();
+      const dataPageName = 'D_OperatorAccessGroups';
+      const serverUrl = this.getBaseUrl();
+      const appAlias = this.getSdkConfigServer().appAlias;
+      const appAliasPath = appAlias ? `app/${appAlias}/` : '';
+      const arExcludedPortals = serverConfig['excludePortals'];
+      let headers = {
+        Authorization: Utils.sdkGetAuthHeader(),
+        'Content-Type': 'application/json',
+      };
 
-    const userAccessGroup = this.PCore.getEnvironmentInfo().getAccessGroup();
-    const dataPageName = 'D_OperatorAccessGroups';
-    const serverUrl = this.getBaseUrl();
-    const appAlias = this.getSdkConfigServer().appAlias;
-    const appAliasPath = appAlias ? `app/${appAlias}/` : '';
-    const arExcludedPortals = serverConfig['excludePortals'];
-    let fetchHeaders = {};
-
-    fetchHeaders['Authorization'] = Utils.sdkGetAuthHeader();
-
-    fetchHeaders['Content-Type'] = 'application/json';
-
-    // Using v1 API here as v2 data_views is not able to access same data page currently.  Should move to avoid having this logic to find
-    //  a default portal or constellation portal and rather have Constellation JS Engine API just load the default portal
-    await fetch(`${serverUrl}${appAliasPath}${endpoints.API}${endpoints.DATA}/${dataPageName}`, {
-      method: 'GET',
-      headers: fetchHeaders,
-    })
-      .then((response) => {
-        if (response.ok && response.status === 200) {
-          return response.json();
-        } else {
-          if (response.status === 401) {
-            // Might be either a real token expiration or revoke, but more likely that the "api" service package is misconfigured
-            throw new Error(`Attempt to access ${dataPageName} failed.  The "api" service package is likely not configured to use "OAuth 2.0"`);
-          }
-          throw new Error(`HTTP Error: ${response.status}`);
-        }
+      // Using v1 API here as v2 data_views is not able to access same data page currently.  Should move to avoid having this logic to find
+      //  a default portal or constellation portal and rather have Constellation JS Engine API just load the default portal
+      fetch(`${serverUrl}${appAliasPath}${endpoints.API}${endpoints.DATA}/${dataPageName}`, {
+        method: 'GET',
+        headers,
       })
-      .then(async (agData) => {
-        let arAccessGroups = agData.pxResults;
-        let selectedPortal = null;
+        .then((response) => {
+          if (response.ok && response.status === 200) {
+            return response.json();
+          } else {
+            if (response.status === 401) {
+              // Might be either a real token expiration or revoke, but more likely that the "api" service package is misconfigured
+              throw new Error(`Attempt to access ${dataPageName} failed.  The "api" service package is likely not configured to use "OAuth 2.0"`);
+            }
+            throw new Error(`HTTP Error: ${response.status}`);
+          }
+        })
+        .then(async (agData) => {
+          const arAccessGroups = agData.pxResults;
+          const availablePortals: Array<any> = [];
 
-        for (let ag of arAccessGroups) {
-          if (ag.pyAccessGroup === userAccessGroup) {
-            // Check if default portal works
-            if (!arExcludedPortals.includes(ag.pyPortal)) {
-              selectedPortal = ag.pyPortal;
-            } else {
+          for (let ag of arAccessGroups) {
+            if (ag.pyAccessGroup === userAccessGroup) {
               console.error(
                 `Default portal for current operator (${ag.pyPortal}) is not compatible with SDK.\nConsider using a different operator, adjusting the default portal for this operator, or using "appPortal" setting within sdk-config.json to specify a specific portal to load.`
               );
-              // Find first portal that is not excluded (might work)
               for (let portal of ag.pyUserPortals) {
                 if (!arExcludedPortals.includes(portal.pyPortalLayout)) {
-                  selectedPortal = portal.pyPortalLayout;
-                  break;
+                  availablePortals.push(portal.pyPortalLayout);
                 }
               }
+              break;
             }
-            break;
           }
-        }
-        if (selectedPortal) {
+
           // Found operator's current access group. Use its portal
-          this.setSdkConfigServer('appPortal', selectedPortal);
-          console.log(`Using appPortal: ${serverConfig.appPortal}`);
-        }
-      })
-      .catch((e) => {
-        console.error(e.message);
-        // check specific error if 401, and wiped out if so stored token is stale.  Fetch new tokens.
-      });
+          console.log(`Available portals: ${availablePortals}`);
+
+          resolve(availablePortals);
+        })
+        .catch((e) => {
+          console.error(e.message);
+          // check specific error if 401, and wiped out if so stored token is stale.  Fetch new tokens.
+        });
+    });
   }
 }
