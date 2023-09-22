b6d55b43b76164639f37ed320948e6fc926c3569
tumms2021389 <97188066+tumms2021389@users.noreply.github.com>
2023-09-22 21:34:19 +0530

    Remove RouterModule (#203)
    * Remove RouterModule

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import { importProvidersFrom } from '@angular/core';
 import { APP_BASE_HREF } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app/app-routing.module';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule, HttpClientModule),
-    { provide: APP_BASE_HREF, useValue: '/' },
-    provideAnimations()
-  ]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimations(), { provide: APP_BASE_HREF, useValue: '/' }]
 }).catch((err) => console.error(err));
