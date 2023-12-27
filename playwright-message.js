d4c728e34880cc59f2cbc9948bfd2516c15ca389
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2023-12-27 15:52:11 +0530

    fix: duplicate entries for @pega/auth in package.json

--- playwright-message.js
+++ playwright-message.js
@@ -1,1 +1,2 @@
-console.log('\x1b[33m%s\x1b[0m','Running in headless mode, use `npm run test:headed` for running in headed mode.');
+// eslint-disable-next-line strict
+console.log('\x1b[33m%s\x1b[0m', 'Running in headless mode, use `npm run test:headed` for running in headed mode.');
