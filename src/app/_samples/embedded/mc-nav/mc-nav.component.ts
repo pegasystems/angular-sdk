6e5d9a35c2a21c8982cebb7af6725529caeff536
Vishal <vishal.sharma@in.pega.com>
2024-11-14 19:05:31 +0530

    Upgrade pega/auth & make changes that were made as part of US-637974

--- src/app/_samples/embedded/mc-nav/mc-nav.component.ts
+++ src/app/_samples/embedded/mc-nav/mc-nav.component.ts
@@ -109,13 +109,13 @@ export class MCNavComponent implements OnInit, OnDestroy {
 
     const sdkConfigAuth = await this.scservice.getSdkConfigAuth();
 
-    if (!sdkConfigAuth.mashupClientId && sdkConfigAuth.customAuthType === 'Basic') {
+    if ((sdkConfigAuth.mashupGrantType === 'none' || !sdkConfigAuth.mashupClientId) && sdkConfigAuth.customAuthType === 'Basic') {
       // Service package to use custom auth with Basic
       const sB64 = window.btoa(`${sdkConfigAuth.mashupUserIdentifier}:${window.atob(sdkConfigAuth.mashupPassword)}`);
       sdkSetAuthHeader(`Basic ${sB64}`);
     }
 
-    if (!sdkConfigAuth.mashupClientId && sdkConfigAuth.customAuthType === 'BasicTO') {
+    if ((sdkConfigAuth.mashupGrantType === 'none' || !sdkConfigAuth.mashupClientId) && sdkConfigAuth.customAuthType === 'BasicTO') {
       const now = new Date();
       const expTime = new Date(now.getTime() + 5 * 60 * 1000);
       let sISOTime = `${expTime.toISOString().split('.')[0]}Z`;
@@ -124,6 +124,14 @@ export class MCNavComponent implements OnInit, OnDestroy {
       // Service package to use custom auth with Basic
       const sB64 = window.btoa(`${sdkConfigAuth.mashupUserIdentifier}:${window.atob(sdkConfigAuth.mashupPassword)}:${sISOTime}`);
       sdkSetAuthHeader(`Basic ${sB64}`);
+    }
+
+    if (sdkConfigAuth.mashupGrantType === 'customBearer' && sdkConfigAuth.customAuthType === 'CustomIdentifier') {
+      // Use custom bearer with specific custom parameter to set the desired operator via
+      //  a userIdentifier property.  (Caution: highly insecure...being used for simple demonstration)
+      sdkSetCustomTokenParamsCB(() => {
+        return { userIdentifier: sdkConfigAuth.mashupUserIdentifier };
+      });
     }
 
     // Login if needed, without doing an initial main window redirect
