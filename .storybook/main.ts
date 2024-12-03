4e432a99b53deff762c4af5ac6a764fd07c282ce
Vishal Sharma <vishalshrm539@gmail.com>
2024-12-03 14:47:59 +0530

    Avoid unintended n/w calls (#380)
    Co-authored-by: Vishal <vishal.sharma@in.pega.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/angular';
+import path from 'path';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/app/_components/custom-sdk/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
@@ -11,6 +13,13 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag'
-  }
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve?.alias) {
+      config.resolve.alias['@pega/auth/lib/sdk-auth-manager'] = path.resolve(__dirname, '../__mocks__/authManager.tsx');
+    }
+
+    return config;
+  },
 };
 export default config;
