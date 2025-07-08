2cf313f875b8efc0f94210cb4ed908785d5aeddf
Vishal Sharma <vishalshrm539@gmail.com>
2025-07-08 16:21:44 +0530

    Updated Storybook to 8 and moved common PCore mocks (#484)
    * Updated Storybook to 8 and moved coomon PCore mocks
    
    * Spacing issue
    
    * Fix package-lock
    
    * Changed extension of pcoreMock file and updated DXCB to 0.242.16
    
    * Fixed package.lock
    
    ---------
    
    Co-authored-by: Sharma <vishal.sharma@in.pega.com>

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,9 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AngularPConnectService, getSdkComponentMap } from '@pega/angular-sdk-components';
 import { Preview, applicationConfig } from '@storybook/angular';
+import setPCoreMocks from '../__mocks__/pcoreMocks';
+
+setPCoreMocks();
 
 getSdkComponentMap();
 
@@ -33,7 +36,6 @@ const preview: Preview = {
     })
   ],
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
