6c604234461328c7afd1ae7f9c3ce6446c4b18c1
Sharma <vishal.sharma@in.pega.com>
2025-07-29 02:17:41 +0530

    Fixed controls changing on blur, etc.

--- .storybook-sdk/main.ts
+++ .storybook-sdk/main.ts
@@ -3,7 +3,13 @@ import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/app/_components/custom-sdk/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@chromatic-com/storybook'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y'
+  ],
   framework: {
     name: '@storybook/angular',
     options: {}
