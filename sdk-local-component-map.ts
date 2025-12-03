ff36b94823f29f950555c9b1bfb783580f7b98f7
Sharma <vishal.sharma@in.pega.com>
2025-12-03 16:48:55 +0530

    Added footer component for the new WSS design and other pieces to get it working

--- sdk-local-component-map.ts
+++ sdk-local-component-map.ts
@@ -1,11 +1,13 @@
 // Statically load all "local" components that aren't yet in the npm package
 
+// import { AppShellComponent } from './src/app/mediaco/overrides/app-shell/app-shell.component';
 /* import end - DO NOT REMOVE */
 
 // localSdkComponentMap is the JSON object where we'll store the components that are
 // found locally. If not found here, we'll look in the Pega-provided component map
 
 const localSdkComponentMap = {
+  // AppShell: AppShellComponent
   /* map end - DO NOT REMOVE */
 };
 
