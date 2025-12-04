51cbab43339d4c7007dca4b06f998fbdd5124d5b
Sharma <vishal.sharma@in.pega.com>
2025-12-04 15:05:40 +0530

    Added component map for mediaco

--- sdk-local-component-map.ts
+++ sdk-local-component-map.ts
@@ -1,13 +1,20 @@
 // Statically load all "local" components that aren't yet in the npm package
 
-// import { AppShellComponent } from './src/app/_mediaco/overrides/app-shell/app-shell.component';
+import sdkMediaCoComponentMap from 'sdk-mediaco-component-map';
+
+import sdkConfig from './sdk-config.json';
+
+import { BannerPageComponent } from './src/app/_components/override-sdk/template/banner-page/banner-page.component';
 /* import end - DO NOT REMOVE */
 
 // localSdkComponentMap is the JSON object where we'll store the components that are
 // found locally. If not found here, we'll look in the Pega-provided component map
 
+const isMediaCoApp = sdkConfig.app === 'mediaco';
+
 const localSdkComponentMap = {
-  // AppShell: AppShellComponent
+  ...(isMediaCoApp ? { ...sdkMediaCoComponentMap } : {}),
+  BannerPage: BannerPageComponent
   /* map end - DO NOT REMOVE */
 };
 
