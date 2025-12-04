4cc1f1ccaf242294c3c1fe42614e17f7da20bffc
Sharma <vishal.sharma@in.pega.com>
2025-12-04 15:05:40 +0530

    Added component map for mediaco

---
+++ sdk-mediaco-component-map.ts
@@ -0,0 +1,14 @@
+// Statically load all "MediaCo" components.
+
+import { AppShellComponent } from './src/app/_mediaco/overrides/app-shell/app-shell.component';
+/* import end - DO NOT REMOVE */
+
+// sdkMediaCoComponentMap is the JSON object where we'll store the components that are
+// specific to MediaCo application.
+
+const sdkMediaCoComponentMap = {
+  AppShell: AppShellComponent
+  /* map end - DO NOT REMOVE */
+};
+
+export default sdkMediaCoComponentMap;
