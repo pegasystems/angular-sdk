09131a088002dfcfca690a4f55487982deaea05e
samhere06 <sameeryamdi@gmail.com>
2025-12-05 16:35:04 +0530

    Overridden appshell,wssnavbar and banner to implement new material navigation bar

--- sdk-mediaco-component-map.ts
+++ sdk-mediaco-component-map.ts
@@ -1,13 +1,18 @@
 // Statically load all "MediaCo" components.
 
-import { AppShellComponent } from './src/app/_mediaco/overrides/app-shell/app-shell.component';
+import { AppShellComponent } from 'src/app/_samples/mediaco/overrides/app-shell/app-shell.component';
+import { BannerComponent } from 'src/app/_samples/mediaco/overrides/banner/banner.component';
+import { WssNavBarComponent } from 'src/app/_samples/mediaco/overrides/wss-nav-bar/wss-nav-bar.component';
+
 /* import end - DO NOT REMOVE */
 
 // sdkMediaCoComponentMap is the JSON object where we'll store the components that are
 // specific to MediaCo application.
 
 const sdkMediaCoComponentMap = {
-  AppShell: AppShellComponent
+  AppShell: AppShellComponent,
+  WssNavBar: WssNavBarComponent,
+  Banner: BannerComponent
   /* map end - DO NOT REMOVE */
 };
 
