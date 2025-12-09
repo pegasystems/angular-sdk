0f4df7bfe019b1f1ab84e5de0f61483c846e1502
tammy-a11y <yuvanagasai.tammu@in.pega.com>
2025-12-09 12:04:08 +0530

    feat: Mediaco Listview in both table and gallery (#603)
    * feat: Mediaco Listview in both table and gallery
    
    * chore: update @pega/angular-sdk-components and @pega/angular-sdk-overrides to version 0.25.3
    
    ---------
    
    Co-authored-by: Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>

--- src/app/_samples/mediaco/sdk-mediaco-component-map.ts
+++ src/app/_samples/mediaco/sdk-mediaco-component-map.ts
@@ -1,9 +1,9 @@
 // Statically load all "MediaCo" components.
-
-import { AppShellComponent } from 'src/app/_samples/mediaco/components/app-shell/app-shell.component';
-import { BannerComponent } from 'src/app/_samples/mediaco/components/banner/banner.component';
-import { WssNavBarComponent } from 'src/app/_samples/mediaco/components/wss-nav-bar/wss-nav-bar.component';
-import { QuickCreateComponent } from 'src/app/_samples/mediaco/components/quick-create/quick-create.component';
+import { AppShellComponent } from './components/app-shell/app-shell.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ListViewComponent } from './components/list-view/list-view.component';
+import { QuickCreateComponent } from './components/quick-create/quick-create.component';
+import { WssNavBarComponent } from './components/wss-nav-bar/wss-nav-bar.component';
 
 /* import end - DO NOT REMOVE */
 
@@ -12,9 +12,10 @@ import { QuickCreateComponent } from 'src/app/_samples/mediaco/components/quick-
 
 const sdkMediaCoComponentMap = {
   AppShell: AppShellComponent,
-  WssNavBar: WssNavBarComponent,
   Banner: BannerComponent,
-  QuickCreate: QuickCreateComponent
+  ListView: ListViewComponent,
+  QuickCreate: QuickCreateComponent,
+  WssNavBar: WssNavBarComponent
   /* map end - DO NOT REMOVE */
 };
 
