8396bc30e0ed49d60275030ff02e7ad832a00a6b
sambhu <sambhu.ss@pega.com>
2025-12-09 16:48:39 +0530

    Removed comments and added changes to run OOTB Listview

--- sdk-local-component-map.ts
+++ sdk-local-component-map.ts
@@ -1,13 +1,13 @@
 // Statically load all "local" components that aren't yet in the npm package
 
-// import sdkMediaCoComponentMap from './src/app/_samples/mediaco/sdk-mediaco-component-map';
+import sdkMediaCoComponentMap from './src/app/_samples/mediaco/sdk-mediaco-component-map';
 /* import end - DO NOT REMOVE */
 
 // localSdkComponentMap is the JSON object where we'll store the components that are
 // found locally. If not found here, we'll look in the Pega-provided component map
 
 const localSdkComponentMap = {
-  // ...sdkMediaCoComponentMap
+  ...sdkMediaCoComponentMap
   /* map end - DO NOT REMOVE */
 };
 
